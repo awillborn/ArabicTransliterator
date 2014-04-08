e4387b4adcfbb1db2f968820157dc3752370b6d7
Anne Willborn <awillborn@gmail.com>
2014-04-08 14:46:53 -0700

    Add getTransliterations function
    Add basic regex functionality. Replaces 'u' in a search term with 'o' and searches both terms in Google Scholar

--- popup.js
+++ popup.js
@@ -1,15 +1,22 @@
 function addSubmitListener() {
   var ele = document.getElementById("search_form");
   if (ele.addEventListener) {
-    ele.addEventListener("submit", openNewTab, false);
+    ele.addEventListener("submit", getTransliterations, false);
   } else if (ele.attachEvent) {
-    ele.attachEvent('onsubmit', openNewTab);
+    ele.attachEvent('onsubmit', getTransliterations);
   }
 }
 
-function openNewTab() {
+function getTransliterations() {
   var searchField = document.getElementById("search_field")
-  var searchTerm = searchField.value
+  var userInput = searchField.value
+  var uRegEx = /u/
+  var newTerm = userInput.replace(uRegEx, "o")
+  var searchTerm = userInput + " OR " + newTerm
+  openNewTab(searchTerm)
+}
+
+function openNewTab(searchTerm) {
   chrome.tabs.create({url: "http://www.scholar.google.com/scholar?q=" + searchTerm})
 }
 
